e82dcbef3f782834e6aba727cc747b609a19ac5d
WSchaijk <wschayk@gmail.com>
2018-07-26 14:58:38 +0200

    Init

---
+++ AppManager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AppManager
+//
+//  Created by Wesley van Schaijk on 26/07/2018.
+//  Copyright © 2018 CSPConnections. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AppManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AppManager
+//
+//  Created by Wesley van Schaijk on 26/07/2018.
+//  Copyright © 2018 CSPConnections. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
